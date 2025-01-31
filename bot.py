f63a1171e2ea9dcfabe449cf77ccb9706cc71ed2
Otabek-lab <90315664+Otabek-lab@users.noreply.github.com>
2025-01-31 19:44:39 +0500

    Update bot.py

--- bot.py
+++ bot.py
@@ -120,9 +120,10 @@ if __name__ == "__main__":
     import asyncio
 
     try:
-        asyncio.run(main())
+        loop = asyncio.get_running_loop()
     except RuntimeError:
-        loop = asyncio.get_event_loop()
-        loop.create_task(main())
-        loop.run_forever()
+        loop = asyncio.new_event_loop()
+        asyncio.set_event_loop(loop)
+
+    loop.run_until_complete(main())
 
